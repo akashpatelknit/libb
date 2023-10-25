f2395d48f7b194a57b60693b8a01d00059b8227d
akashpatelknit <cotsec14@gmail.com>
2023-10-25 15:31:34 +0530

    blog page modified and text editiroe added

--- src/components/Blog/BlogCard.jsx
+++ src/components/Blog/BlogCard.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Blog.scss';
+import './BlogCard.scss';
 
-const BlogCard = ({ title, url, author, datePublished, slug }) => {
+const BlogCard = ({ title, url, author, datePublished }) => {
+	title =
+		'The chief technology officer (CTO) is the individual within an organization who oversees the current technology and creates relevant policy. A CTO should have the busine';
+	author = 'Author Name';
+	datePublished = 'Date Published';
+	url =
+		'https://images.unsplash.com/photo-1698167646833-b705453b235e?auto=format&fit=crop&q=80&w=1932&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 	return (
-		<div class="container">
-			<div class="card">
-				<div class="card-header">
-					<Link to={'/posts/' + slug}>
-						<img src={url} alt="rover" />
-					</Link>
+		<Link to="/blog">
+			<div className="blog-card">
+				<div className="blog__card__image">
+					<img src={url} alt="Blog" className="blog-image" />
 				</div>
-				<div class="card-body">
-					<span class="tag tag-teal">Technology</span>
-					<h4>{title}</h4>
-					<div class="user">
-						<img src={author.avatar.url} alt="user" />
-						<div class="user-info">
-							<h5>{author.name}</h5>
-							<small>{datePublished}</small>
-						</div>
+				<div className="textContainer">
+					<div className="blog-title">
+						<span>{title}</span>
+					</div>
+					<div className="blog-date">
+						<span>{author}</span>
+						<span>{datePublished}</span>
 					</div>
 				</div>
 			</div>
-		</div>
+		</Link>
 	);
 };
 
--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -5,22 +5,25 @@ import { Input } from '@chakra-ui/react';
 const Example = ({ placeholder }) => {
 	const editor = useRef(null);
 	const [content, setContent] = useState('');
+	console.log(content)
 	return (
 		<main className="editor">
+			<span dangerouslySetInnerHTML={{ __html: content }} />
 			<div className="input-item">
 				<label htmlFor="title">Title</label>
 				<Input type="text" width={'100%'} />
 				<label htmlFor="title">Description</label>
-				<Input type="text" width={'100%'} px={'5px'}/>
+				<Input type="text" width={'100%'} px={'5px'} />
 				<label htmlFor="title">Background Image</label>
-				<Input type="image" width={'100%'} />
+				<Input type="file" width={'100%'} />
 				<label htmlFor="title">Post Content</label>
 			</div>
+
 			<JoditEditor
 				ref={editor}
 				value={content}
 				tabIndex={1} // tabIndex of textarea
-				onBlur={(newContent) => setContent(newContent)} 
+				onChange={(newContent) => setContent(newContent)}
 			/>
 		</main>
 	);
